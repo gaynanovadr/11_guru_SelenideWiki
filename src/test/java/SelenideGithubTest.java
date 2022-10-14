2f62019864b35dbb3273bf3789efa67742c71c14
dinara <gaynanovadr@mail.ru>
2022-10-14 18:48:54 +0200

    added test

---
+++ src/test/java/SelenideGithubTest.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class SelenideGithubTest {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @DisplayName("Checking for JUnit5 sample code on the SoftAssertions page on Github")
+    @Test
+    void selenideWikiOnGithubSearchTest() {
+        open("https://github.com");
+        $("input[type='text']").setValue("selenide").pressEnter();
+        $$(".repo-list li").first().$("a").click();
+        $("[id=wiki-tab]").click();
+        $("input[placeholder='Find a page…']").setValue("SoftAssertions");
+        $(byText("SoftAssertions")).click();
+        $("[id=wiki-wrapper]").shouldHave(text("Using JUnit4"));
+
+    }
+
+}
